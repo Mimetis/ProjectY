aaa94e5d3e75227678e01968683ed8950bf74004
Jean-Sébastien Dupuy <jdupuy@microsoft.com>
2021-05-20 14:53:16 +0200

    Updating YStorage class documentation

--- Ygdra.Python/ygdra/storage.py
+++ Ygdra.Python/ygdra/storage.py
@@ -1,11 +1,27 @@
 class YStorage:
 
     def __init__(self, storage_name: str) -> None:
+        """
+        YStorage constructor.
+
+        Parameters
+        ----------
+        storage_name : str
+            Storage name.
+        """
+
         self.storage_name = storage_name
 
     def get_path(self, container: str, path: str) -> str:
         """
-        Get full data lake storage path
+        Get full data lake storage path.
+
+        Parameters
+        ----------
+        container : str
+            Storage container.
+        path: str
+            Storage relative path.
         """
         if not container:
             raise ValueError('container input value is null or empty')
